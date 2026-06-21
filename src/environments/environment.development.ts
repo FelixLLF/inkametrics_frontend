992420dd77d47728a1146c4b7e5673c22da2cc35
Félix Luis López <fl.lopez.321@gmail.com>
2026-06-21 04:11:26 -0500

    environment modificado para producción

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment={
-    production:false,
-    base:'http://localhost:8080'
+    // production:false,
+    // base:'http://localhost:8080'
+    production:true,
+    base:'https://inkametrics.onrender.com'
+    
 }
