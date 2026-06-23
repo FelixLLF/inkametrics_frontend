efe1724998c7fa9d67916acdf839301078f71a86
Félix Luis López <fl.lopez.321@gmail.com>
2026-06-22 21:42:22 -0500

    solución a error de environment

--- src/app/services/canalmonitoreado-service.ts
+++ src/app/services/canalmonitoreado-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { CanalMonitoreado } from '../models/CanalMonitoreado';
 
 const base_url = environment.base;
